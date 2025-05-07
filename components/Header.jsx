3fef389b97a5d00c435d5d373eca600bb5c0eea6
MohamadRiza <rawufdeenriza@gmail.com>
2025-05-07 15:25:31 +0530

    Header Half

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,42 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
+import { SignedIn } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { CarFront } from "lucide-react";
 
-const Header = () => {
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false;
+
   return (
-    <div>
-      Header
-    </div>
+    <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b ">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href={isAdmin ? "/admin" : "/"}>
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+          />
+          {isAdminPage && (
+            <span className="text-xs font-extralight">admin</span>
+          )}
+        </Link>
+
+        <div>
+          <SignedIn>
+            <Link href="/saved-cars">
+              <Button>
+                <CarFront size={18} />
+
+                <span className="hidden md:inline">Saved Cars</span>
+              </Button>
+            </Link>
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
   );
 };
 
