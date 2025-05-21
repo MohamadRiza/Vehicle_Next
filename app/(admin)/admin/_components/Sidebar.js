7cdbc39d7c977c7a99152e47bbf18a0f206d705c
MohamadRiza <rawufdeenriza@gmail.com>
2025-05-21 14:29:32 +0530

    mobile nav sidebar admin page

--- app/(admin)/admin/_components/Sidebar.js
+++ app/(admin)/admin/_components/Sidebar.js
@@ -42,7 +42,9 @@ const Sidebar = () => {
               href={route.href}
               className={cn(
                 "flex items-center gap-x-2 text-slate-500 text-sm font-medium pl-6 transition-all hover:text-slate-600 hover:bg-slate-100/50 h-12",
-                pathname === route.href ? "text-blue-700 bg-blue-100/50 hover:bg-blue-100 hover:text-blue-700" : ""
+                pathname === route.href
+                  ? "text-blue-700 bg-blue-100/50 hover:bg-blue-100 hover:text-blue-700"
+                  : ""
               )}
             >
               <route.icon className="h-5 w-5" />
@@ -51,7 +53,26 @@ const Sidebar = () => {
           );
         })}
       </div>
-      <div></div>
+
+      <div className="md:hidden fixed bottom-0 left-0 right-0 z-50 bg-white border-t flex justify-around items-center h-16">
+        {routes.map((route) => {
+          return (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "flex flex-col items-center justify-center text-slate-500 text-xs font-medium transition-all py-1 flex-1",
+                pathname === route.href
+                  ? "text-blue-700 "
+                  : ""
+              )}
+            >
+              <route.icon className="h-5 w-5" />
+              {route.label}
+            </Link>
+          );
+        })}
+      </div>
     </>
   );
 };
