eecd9f3e60a21929d62d9e40c8d6f85c46f8124d
MohamadRiza <rawufdeenriza@gmail.com>
2025-05-21 13:34:59 +0530

    sidebar and navigations for admin

--- app/(admin)/admin/_components/Sidebar.js
+++ app/(admin)/admin/_components/Sidebar.js
@@ -1,7 +1,9 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { Calendar, Car, Cog, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const routes = [
@@ -28,12 +30,21 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <>
       <div className="hidden md:flex h-full flex-col overflow-y-auto bg-white shadow-sm border-r">
         {routes.map((route) => {
           return (
-            <Link key={route.href} href={route.href}>
+            <Link
+              key={route.href}
+              href={route.href}
+              className={cn(
+                "flex items-center gap-x-2 text-slate-500 text-sm font-medium pl-6 transition-all hover:text-slate-600 hover:bg-slate-100/50 h-12",
+                pathname === route.href ? "text-blue-700 bg-blue-100/50 hover:bg-blue-100 hover:text-blue-700" : ""
+              )}
+            >
               <route.icon className="h-5 w-5" />
               {route.label}
             </Link>
