c79d72f428d33d0a51627217ab6b7e95a79251ef
MohamadRiza <rawufdeenriza@gmail.com>
2025-05-21 10:49:26 +0530

    admin page dashboard logos

--- app/(admin)/admin/_components/Sidebar.js
+++ app/(admin)/admin/_components/Sidebar.js
@@ -1,13 +1,48 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { Calendar, Car, Cog, LayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/admin",
+  },
+  {
+    label: "Cars",
+    icon: Car,
+    href: "/admin/cars",
+  },
+  {
+    label: "Test Drive",
+    icon: Calendar,
+    href: "/admin/test-drive",
+  },
+  {
+    label: "Settings",
+    icon: Cog,
+    href: "/admin/settings",
+  },
+];
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebars
-    </div>
-  )
-}
+    <>
+      <div className="hidden md:flex h-full flex-col overflow-y-auto bg-white shadow-sm border-r">
+        {routes.map((route) => {
+          return (
+            <Link key={route.href} href={route.href}>
+              <route.icon className="h-5 w-5" />
+              {route.label}
+            </Link>
+          );
+        })}
+      </div>
+      <div></div>
+    </>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
