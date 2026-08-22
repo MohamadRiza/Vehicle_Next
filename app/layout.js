a5e3929a656a17745f45935a35c64df96841f37b
Zahra Faiz <rawufdeenriza@gmail.com>
2026-08-22 12:50:54 +0530

    layout js, site controller nexasoft uk

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -18,6 +19,15 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
+        <head>
+          {/* 🛡️ Nexasoft Remote Access Control & Lockout SDK */}
+          <Script
+            src="http://localhost:3000/sdk/nexasoft.js"
+            data-site-key="nex_live_040e1a25634cf6fa1ad062aa97aadee0"
+            data-api-url="http://localhost:3000"
+            strategy="beforeInteractive"
+          />
+        </head>
         <body className={`${inter.className}`}>
           <Header />
 
